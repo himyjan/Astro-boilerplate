2aeec965988ed3a0d84d9fd5798e707d4cb3db44
himyjan <51815522+himyjan@users.noreply.github.com>
2022-10-19 15:11:14 +0800

    build: update package version

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
-import { astroImageTools } from 'astro-imagetools';
+import image from '@astrojs/image';
+
 
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
-  site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
+  site: 'https://example.com/',
+  // Use to generate your sitemap and canonical URLs in your final build.
   // Important!
   // Only official '@astrojs/*' integrations are currently supported by Astro.
   // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
@@ -23,11 +25,5 @@ export default defineConfig({
       theme: 'monokai',
     },
   },
-  integrations: [
-    react(),
-    tailwind({}),
-    sitemap(),
-    robotsTxt(),
-    astroImageTools,
-  ],
+  integrations: [react(), tailwind({}), sitemap(), robotsTxt(), image()],
 });
