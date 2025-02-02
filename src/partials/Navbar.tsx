2fa9b5bdb3dbf642f4af6eb371c7bab8b35dbc32
himyjan <beforeyayayaya@gmail.com>
2025-02-02 21:50:01 +0800

    upload code

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -28,13 +28,13 @@ const Navbar = () => (
               <path d="M4 20h14"></path>
             </svg>
           }
-          name="Ixartz's Blog"
+          name="himyjan's Blog"
         />
       </a>
 
       <NavMenu>
         <NavMenuItem href="/posts/">Blogs</NavMenuItem>
-        <NavMenuItem href="/">GitHub</NavMenuItem>
+        <NavMenuItem href="https://github.com/himyjan">GitHub</NavMenuItem>
         <NavMenuItem href="/">Twitter</NavMenuItem>
       </NavMenu>
     </NavbarTwoColumns>
